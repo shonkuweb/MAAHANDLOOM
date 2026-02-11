7d5e2220bc109ae602ad830ddee1ea30be10cf73
Indrita Fabrics <admin@indritafabrics.com>
2026-02-11 10:03:32 +0530

    Update footer email to subirchanda43@gmail.com

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,7 +53,7 @@ const Footer = () => {
                                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
                                 <polyline points="22,6 12,13 2,6"></polyline>
                             </svg>
-                            <a href="mailto:info@inditrafabrics.com">info@inditrafabrics.com</a>
+                            <a href="mailto:subirchanda43@gmail.com">subirchanda43@gmail.com</a>
                         </li>
                     </ul>
 
