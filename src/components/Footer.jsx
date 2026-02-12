2b06cc935f693579c262c2ba9b32f7c2c72f5e93
Indrita Fabrics <admin@indritafabrics.com>
2026-02-12 08:56:12 +0530

    Add physical address to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -55,6 +55,13 @@ const Footer = () => {
                             </svg>
                             <a href="mailto:indritafabrics@gmail.com">indritafabrics@gmail.com</a>
                         </li>
+                        <li style={{ alignItems: 'flex-start' }}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" style={{ marginTop: '4px' }}>
+                                <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path>
+                                <circle cx="12" cy="10" r="3"></circle>
+                            </svg>
+                            <span>Gaighata, Baikara, Bongaon, North 24 Parganas, West Bengal, 743245</span>
+                        </li>
                     </ul>
 
                     <div className="footer-social">
